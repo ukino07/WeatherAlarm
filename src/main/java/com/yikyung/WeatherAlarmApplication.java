4dfc7fd0e85662150de0c52770f981a6ed893f4c
이경 <u_kino07@tmon.co.kr>
2018-04-20 20:26:33 +0900

    초기 셋팅

---
+++ src/main/java/com/yikyung/WeatherAlarmApplication.java
@@ -0,0 +1,12 @@
+package com.yikyung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAlarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAlarmApplication.class, args);
+	}
+}
---
+++ src/test/java/com/yikyung/WeatherAlarmApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yikyung;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeatherAlarmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
